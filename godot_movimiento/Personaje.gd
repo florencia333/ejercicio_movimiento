0fd4128fe12accb248192aaae92b8b6dc7efc35b
Florencia Belate Dietrich <flormadriguera@gmail.com>
2024-04-15 18:43:22 -0300

    doble salto
    me falta ponerle limite al doble salto equisde

--- godot_movimiento/Personaje.gd
+++ godot_movimiento/Personaje.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 450.0
-const JUMP_VELOCITY = -500.0
+const SPEED = 600.0
+const JUMP_VELOCITY = -1050.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +14,25 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	var double_jumps_available = 1
+	
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor() or double_jumps_available > 0:
+			if not is_on_floor():
+				double_jumps_available -= 1
+			velocity.y = JUMP_VELOCITY
+		
+	if is_on_floor():
+		double_jumps_available = 1
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		
+	
+	
 	move_and_slide()
